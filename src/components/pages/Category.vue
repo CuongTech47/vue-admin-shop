3ee89e340f2536ac65fa1d3a093634878f08d449
Ngoc Cuong <cuongtech47@gmail.com>
2021-09-07 18:07:00 +0700

    add category

--- src/components/pages/Category.vue
+++ src/components/pages/Category.vue
@@ -2,10 +2,16 @@
   <div>
     <div class="content">
       <div class="container-fluid">
-        
         <!--~~~~~~~ TABLE ONE ~~~~~~~~~-->
-        <div class="_1adminOverveiw_table_recent _box_shadow _border_radious _mar_b30 _p20">
-         <p class="_title0">Category <Button ><Icon type="md-add" /> Add tag</Button></p>
+        <div
+          class="_1adminOverveiw_table_recent _box_shadow _border_radious _mar_b30 _p20"
+        >
+          <p class="_title0">
+            Category
+            <Button @click="addModal = true"
+              ><Icon type="md-add" /> Add tag</Button
+            >
+          </p>
 
           <div class="_overflow _table_div">
             <table class="_table">
@@ -19,19 +25,18 @@
               </tr>
               <!-- TABLE TITLE -->
 
-
               <!-- ITEMS -->
-              <tr v-for="(cat , i) in cats" :key="i" >
-                <td>{{cat.category_id}}</td>
-                <td class="_table_name">{{cat.category_name}}</td>
-                <td v-if="cat.category_status==0" >An</td>
-                <td v-if="cat.category_status==1" >Hien thi</td>
-                 <td>{{cat.created_at}}</td>
+              <tr v-for="(cat, i) in cats" :key="i">
+                <td>{{ cat.category_id }}</td>
+                <td class="_table_name">{{ cat.category_name }}</td>
+                <td v-if="cat.category_status == 0">An</td>
+                <td v-if="cat.category_status == 1">Hien thi</td>
+                <td>{{ cat.created_at }}</td>
                 <td>
-                    <Button type="info" size="small"  >Edit</Button>
-					<Button type="error" size="small"  >Delete</Button>
+                  <Button type="info" size="small" @click="showEditModal(cat, i)">Edit</Button>
+                  <Button type="error" size="small">Delete</Button>
                   <!-- <el-button type="primary" icon="el-icon-edit" @click="showEditModal(cat,i)" size="mini">Edit</el-button>
-                  <el-button type="danger" icon="el-icon-delete" @click="showDeletingModal(cat, i)" size="mini">Clear</el-button> --> 
+                  <el-button type="danger" icon="el-icon-delete" @click="showDeletingModal(cat, i)" size="mini">Clear</el-button> -->
                 </td>
               </tr>
             </table>
@@ -39,37 +44,64 @@
         </div>
         <!-- ADD CATEGORY MODAL -->
         <Modal
-			v-model="addModal"
-			title="Add tag"
-			:mask-closable="false"
-			:closable="false"
-			>
-			<Input v-model="data.tagName" placeholder="Add tag name"  />
+          v-model="addModal"
+          title="Add Category"
+          :mask-closable="false"
+          :closable="false"
+        >
+          <Form v-model="data" :label-width="80">
+            <FormItem label="Name">
+              <Input v-model="data.category_name" placeholder="Enter your name"></Input>
+            </FormItem>
+            <FormItem label="Desc">
+              <Input v-model="data.category_desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
+            </FormItem>
+            <FormItem label="Trang Thai">
+              <Select v-model="data.category_status" placeholder="chon trang thai hien thi">
+                  <Option value="0">An</Option>
+                  <Option value="1">Hien Thi</Option>
+              </Select>
+            </FormItem>
+          </Form>
+          <div slot="footer">
+            <Button type="default" @click="addModal= false">Close</Button>
+            <Button
+              type="primary"
+              @click="add_category"
+              :disabled="isAdding"
+              :loading="isAdding"
+              >{{ isAdding ? "Adding.." : "Add Category" }}</Button>
+          </div>
+        </Modal>
 
-			<div slot="footer">
-				<Button type="default" @click="addModal=false">Close</Button>
-				<Button type="primary" @click="addTag" :disabled="isAdding" :loading="isAdding">{{isAdding ? 'Adding..' : 'Add tag'}}</Button>
-			</div>
-
-		</Modal>
-        <!-- Edit CATEGORY MODAL -->
+        <!-- category editing modal -->
         <Modal
-			v-model="editModal"
-					title="Edit tag"
+					v-model="editModal"
+					title="Edit Category"
 					:mask-closable="false"
 					:closable="false"
-
 					>
-					<Input v-model="editData.tagName" placeholder="Edit tag name"  />
+					<Form v-model="editData" :label-width="80">
+            <FormItem label="Name">
+              <Input v-model="editData.category_name" placeholder="Enter your name"></Input>
+            </FormItem>
+            <FormItem label="Desc">
+              <Input v-model="editData.category_desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
+            </FormItem>
+            <FormItem label="Trang Thai">
+              <select v-model="editData.category_status" >
+                  <option value="0">An</option>
+                  <option value="1">Hien thi</option>
+              </select>
+            </FormItem>
+          </Form>
 
 					<div slot="footer">
 						<Button type="default" @click="editModal=false">Close</Button>
-						<Button type="primary" @click="editTag" :disabled="isAdding" :loading="isAdding">{{isAdding ? 'Editing..' : 'Edit tag'}}</Button>
+						<Button type="primary" @click="edit_category" :disabled="isAdding" :loading="isAdding">{{isAdding ? 'Editing..' : 'Edit Category'}}</Button>
 					</div>
 
-		</Modal>
-
-        
+				</Modal>
       </div>
     </div>
   </div>
@@ -78,176 +110,128 @@
 <script>
 import axios from "axios";
 export default {
-    data(){
-        return {
-            data:{
-            category_name:"",
-            category_desc:"",
-            category_status:"",
-        },
-        formLabelWidth: '120px',
-        addModal: false,
-        editModal:false,
-        isAdding:false,
-        cats:[],
-        editData:{
-            category_name:"",
-            category_desc:"",
-            category_status:"",
-        },
-        index : -1,
-        deletingIndex: -1,
-        isDeleing : false,
-        showDeleteModal: false,
-        deleteItem: {}, 
-         
+  data() {
+    return {
+      data: {
+        category_name: "",
+        category_desc: "",
+        category_status: "",
+      },
+      addModal: false,
+      editModal: false,
+      isAdding: false,
+      cats: [],
+      editData: {
+        category_name: "",
+        category_desc: "",
+        category_status: "",
+      },
+      index: -1,
+      deletingIndex: -1,
+      isDeleing: false,
+      showDeleteModal: false,
+      deleteItem: {},
+    };
+  },
+
+  methods: {
+    async add_category() {
+        if (this.data.category_name.trim() == "")
+          return this.e("Category name is required");
+        if (this.data.category_desc.trim() == "")
+          return this.e("Category desc is required");
+        if (this.data.category_status.trim() == "")
+          return this.e("Category status is required");
+      const res = await axios.post("http://localhost:8000/api/v1/category",this.data);
+      if(res.status === 201) {
+        this.cats.unshift(res.data.data);
+        this.s("Tag has been added successfully!");
+        this.addModal = false;
+        this.data= "";
+      } else {
+        if(res.status==422){
+					if(res.data.errors.category_name){
+						this.e(res.data.errors.category_name[0])
+					}
+					
+				}else{
+					this.swr()
+				}
       }
     },
-
-    methods:{
-      s(mes , title="Great!") {
-        this.$notify({
-          title: title,
-          message: mes,
-          type: 'success'
-        });
-      },
-
-      w(mes , title="Oops!") {
-        this.$notify({
-          title: title,
-          message: mes,
-          type: 'warning'
-        });
-      },
-
-      swr(mes="Something went wrong! Please try again" , title="Oops") {
-        this.$notify.error({
-          title: title,
-          message: mes
-        });
-      },
-
-      e(mes , title="Oops!") {
-        this.$notify.error({
-          title: title,
-          message: mes
-        });
-      },
-        
-      async add_category(){
-        if(this.data.category_name.trim()=="") return this.e("Category Name is required")
-        if(this.data.category_desc.trim()=="") return this.e("Category Desc is required")
-        if(this.data.category_status.trim()=="") return this.e("Category Status is required")
-        const res = await axios.post('http://localhost:8080/api/v1/category',this.data)
-        if(res.status===201){
-          this.cats.unshift(res.data)
-          this.s('them thanh cong')
-          this.addModal = false
-          this.data = ""
-        }else{
-            if(res.status==422){
-              console.log(res.data)
-              if(res.data.errors.tagName){
-                this.e(res.data.errors.tagName[0])
-              }
-              
-            }else{
-              this.swr()
-          }
-         
+    async edit_category() {
+        if (this.editData.category_name.trim() == "")
+          return this.e("Category name is required");
+        if (this.editData.category_desc.trim() == "")
+          return this.e("Category desc is required");
+        if(this.editData.category_status.trim() == "")
+          return this.e("Category status is required");
+      const res = await axios.patch(`http://localhost:8000/api/v1/category/${this.editData.category_id}`,this.editData)
+      if (res.status === 200) {
+        this.cats[this.index].category_name = this.editData.category_name;
+        this.cats[this.index].category_desc = this.editData.category_desc;
+        this.cats[this.index].category_status = this.editData.category_status;
+        this.s("sua thanh cong");
+        this.editModal = false;
+        this.editData = "";
+      } else {
+        if (res.status === 422) {
+          console.log(res.data.category_name);
+          if(res.data.errors.category_name)
+              this.i(res.data.errors.category_name[0]) 
+        } else {
+          this.swr();
         }
-          
-      },
-      async edit_category(){
-        if(this.editData.category_name.trim()=="") return this.e("Category Name is required")
-        if(this.editData.category_desc.trim()=="") return this.e("Category Desc is required")
-        if(this.editData.category_status.trim()=="") return this.e("Category Status is required")
-        const res = await axios.patch(`http://localhost:5000/api/v1/category/update/${this.editData._id}`,this.editData)
-        if(res.status===200){
-          this.cats[this.index].category_name = this.editData.category_name
-          this.cats[this.index].category_desc = this.editData.category_desc
-          this.cats[this.index].category_status = this.editData.category_status
-          this.s('sua thanh cong')
-          this.editModal = false
-          this.editData = ""
-         
-        }else{
-          if(res.status===422){
-            console.log(res.data.message)
-            /* if(res.data.errors.category_name)
-              this.i(res.data.errors.category_name[0])  */
-           }else{
-             this.swr()
-          }
-         
-        }
-          
-      },
-        async delete_category(){
-          this.isDeleing = true
-          const res = await axios.patch(`http://localhost:5000/api/v1/category/update/${this.deleteItem._id}`,this.deleteItem)
-          if(res.status===200){
-            this.cats.splice(this.deletingIndex,1)
-            this.s('Xoa!')
-          }else{
-            this.swr()
-          }
-          /* this.isDeleing = false
-          this.showDeleteModal = false */
-      },
-        showDeletingModal(cat, i){
-          const deleteModalObj  =  {
-            showDeleteModal: true, 
-            deleteUrl : 'app/delete_tag', 
-            data : cat, 
-            deletingIndex: i, 
-            isDeleted : false,
-          }
-          this.$store.commit('setDeletingModalObj', deleteModalObj)
-          console.log('delete method called')
-          // this.deleteItem = tag
-          // this.deletingIndex = i
-          // this.showDeleteModal = true
-      }, 
-      showEditModal(cat, index){
+      }
+    },
+    showEditModal(cat, index){
 			let obj = {
-				_id : cat._id, 
+        category_id : cat.category_id,
 				category_name : cat.category_name,
         category_desc:cat.category_desc,
         category_status:cat.category_status,
-
 			}
 			this.editData = obj
 			this.editModal = true
 			this.index = index
 		}, 
-      
-      /* add_category_product(){
-      let newCategory = {
-        category_name:this.data.category_name,
-        category_desc:this.data.category_desc,
-        category_status:this.data.category_status
+    async delete_category() {
+      this.isDeleing = true;
+      const res = await axios.patch(
+        `http://localhost:5000/api/v1/category/update/${this.deleteItem._id}`,
+        this.deleteItem
+      );
+      if (res.status === 200) {
+        this.cats.splice(this.deletingIndex, 1);
+        this.s("Xoa!");
+      } else {
+        this.swr();
       }
-      axios.post('http://localhost:5000/api/v1/category/create',newCategory)
-          .then(res=>{
-            console.log(res)
-            this.msg=""
-            this.$router.push('/category')
-          },msg => {
-            console.log(msg.response)
-            this.err.response.data.error
-          })
-    }, */
+      /* this.isDeleing = false
+          this.showDeleteModal = false */
     },
-    async created() {
-        const res = await axios.get('http://localhost:8080/api/v1/category')
-        if(res.status==200){
-          
-          this.cats = res.data.data
-        }else{
-          this.swr()
-        }
-      }
-}
+    showDeletingModal(cat, i) {
+      const deleteModalObj = {
+        showDeleteModal: true,
+        deleteUrl: "app/delete_tag",
+        data: cat,
+        deletingIndex: i,
+        isDeleted: false,
+      };
+      this.$store.commit("setDeletingModalObj", deleteModalObj);
+      console.log("delete method called");
+      // this.deleteItem = tag
+      // this.deletingIndex = i
+      // this.showDeleteModal = true
+    },
+  },
+  async created() {
+    const res = await axios.get("http://localhost:8000/api/v1/category");
+    if (res.status == 200) {
+      this.cats = res.data.data;
+    } else {
+      this.swr();
+    }
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import router from './router'
 import App from './App.vue'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
+import commom from './commom'
 Vue.use(ViewUI);
 /* Vue.config.productionTip = false */
-
+Vue.mixin(commom)
 new Vue({
   render: h => h(App),
   router
